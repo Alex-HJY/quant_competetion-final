34813a9dcf4057af60a15d2cc7190e317c1fc8e5
ALEX-HJY <156979387@qq.com>
2018-12-05 20:15:40 +0800

    2018 12 5

---
+++ getdata.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+from dateutil import parser
+
+
+def get_close_data():
+    """
+    获取不同股票的收盘价
+    :return: 不同股票收盘价  DataFrame
+    """
+    df = pd.DataFrame()
+
+    return df
+
+
+def get_period():
+    """
+    获取经济周期
+    :return: 经济周期 DataFrame
+    """
+    df = pd.DataFrame()
+    return df
+
+
+
+
+if __name__ == '__main__':
+    print(get_close_data())
+
